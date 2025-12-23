70391e7cf5c5f4271dd664be0e1bca8c611289cc
jeadie <jack@spice.ai>
2025-12-23 10:46:17 +1000

    Metadata struct with internal access

--- async-openai/src/types/metadata.rs
+++ async-openai/src/types/metadata.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 /// Set of 16 key-value pairs that can be attached to an object.
 /// This can be useful for storing additional information about the
 /// object in a structured format, and querying for objects via API
@@ -13,5 +15,11 @@ pub struct Metadata(serde_json::Value);
 impl From<serde_json::Value> for Metadata {
     fn from(value: serde_json::Value) -> Self {
         Self(value)
+    }
+}
+
+impl Metadata {
+    pub fn as_value(&self) -> &serde_json::Value {
+        &self.0
     }
 }
